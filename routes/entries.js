d78c80e3366fb9a868ace73f743b0e48e9aa3cf1
Leonardo1596 <leonardoemanuel156@gmail.com>
2025-07-06 03:35:51 -0300

    fixed get entries route

--- routes/entries.js
+++ routes/entries.js
@@ -5,7 +5,7 @@ const EntryController = require('../controllers/EntryController');
 router.post('/entry/create', EntryController.createEntry);
 router.delete('/entry/delete/:userId/:entryId', EntryController.deleteEntry);
 router.put('/entry/update/:userId/:entryId', EntryController.updateEntry);
-// router.get('/entries/:userId', EntryController.getEntryByUser);
+router.get('/get/entries/:userId', EntryController.getEntryByUser);
 router.get('/entries/resume', EntryController.getResumeByPeriod);
 
 module.exports = router;
